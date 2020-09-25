4a3187978e5ff610e0e5368cb43fdd8045542847
Grochoska <rafael@goepik.com.br>
2020-09-25 09:35:59 -0300

    Time Tween v1.0.0

---
+++ Assets/UPM/Runtime/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace Grochoska.TimeTween
+{
+    public static class Timer
+    {
+        public static TimeTween CreateTween(float duration, Action<TimeTween> whileRunning = null,
+            Action onEnd = null)
+        {
+            Debug.Assert(duration != 0, "You can't create a Timer with duration <= 0. Aborting...");
+            if (duration <= 0) return null;
+
+            var countdown = new TimeTween(duration);
+
+            countdown.AddListenerOnEnd(onEnd);
+            if (whileRunning != null) countdown.AddRoutine(whileRunning);
+            return countdown;
+        }
+    }
+}
---
+++ Assets/UPM/Runtime/TimerRoutiner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Grochoska.TimeTween
+{
+    internal class TimerRoutiner : MonoBehaviour
+    {
+        /// <summary>
+        /// Returns the instance of the currently running Timer Routiner
+        /// </summary>
+        internal static TimerRoutiner Instance { get; private set; } = null;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Setup()
+        {
+            if (Instance != null) return;
+
+            var routiner = new GameObject("[TIMER ROUTINER]");
+            DontDestroyOnLoad(routiner.gameObject);
+
+            Instance = routiner.AddComponent<TimerRoutiner>();
+
+            SceneManager.sceneLoaded += (s, mode) =>
+            {
+                if (mode == LoadSceneMode.Single)
+                    Instance.StopAllCoroutines();
+            };
+        }
+    }
+}
